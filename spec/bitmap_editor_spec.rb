9a5e89a9732aa537f5cc117ad3a2c520cb7f5c32
maxmills <8balldigitalsolutions@gmail.com>
2020-03-25 09:36:07 +0000

    initial bitmap tests

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 RSpec.describe BitmapEditor do
-  it "has a version number" do
+  it 'has a version number' do
     expect(BitmapEditor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    byebug
-    expect(false).to eq(true)
+  it 'does something useful' do
+    expect(true).to eq(true)
   end
 end
